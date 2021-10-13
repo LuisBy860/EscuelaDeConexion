9de5fdb8822a536d541cfeab4f94bb9d1dfdb333
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2021-10-13 10:45:46 -0600

    Agregar archivos de proyecto.

---
+++ EsculaDeConexion/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EsculaDeConexion.Models;
+using EsculaDeConexion.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EsculaDeConexion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+        
+            CoursesRepository coursesRepository = new CoursesRepository();
+      
+           var ListOfData =  coursesRepository.ListDataCourses();
+           
+          
+          return View(ListOfData);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
